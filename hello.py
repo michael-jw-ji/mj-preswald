95fc523e1336c262f9f950cfa1e7aef9886206f8
ivan.cheng <ivan.cheng@replicant.ai>
2025-04-06 21:29:57 -0400

    stuff

--- hello.py
+++ hello.py
@@ -1,20 +1,40 @@
 from preswald import text, plotly, connect, get_df, table, query, slider
 import pandas as pd
 import plotly.express as px
+import logging
 
 text("# Welcome to Preswald!")
 text("This is your first app. :tada:")
 
 
 
+# Set up logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create a handler for console output
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(console_handler)
+
+# logger.info("zsdfasdfasdon")
 
 
 # Load the CSV
 connect() # load in all sources, which by default is the sample_csv
 df = get_df('evpd_csv')
-    
+
+# logger.info(df.columns)
+
+
 table(df, title="Full Dataset Preview")
-sql = "SELECT * FROM evpd_csv WHERE Electric Range > 50"
+sql = "SELECT \"Electric Range\" FROM evpd_csv WHERE \"Electric Range\" > 50"
 filtered_df = query(sql, "evpd_csv")
 text("# My Data Analysis App")
 table(filtered_df, title = "Filtered Data")
